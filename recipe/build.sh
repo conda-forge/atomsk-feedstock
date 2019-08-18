1cdc43ec461718d41f559dc7f8cd3cf0fad3ce4c
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-18 22:19:28 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export LAPACK=$PREFIX/lib/liblapack${SHLIB_EXT}
+export LAPACK=${PREFIX}/lib/liblapack${SHLIB_EXT}
 cd src
 make atomsk
-cp atomsk $PREFIX/bin/atomsk
+mkdir -p ${PREFIX}/bin
+cp atomsk ${PREFIX}/bin/atomsk
